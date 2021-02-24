42b0a2ee6a22a063d3c1a5ed5c9cbf9904756787
John Robinson <johnarobinson133@gmail.com>
2021-02-24 08:45:24 -0700

    hiding DB stuff in server app.js

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://jrobi133:Hunter15@workoutdb.izarp.mongodb.net/WorkoutDB?retryWrites=true&w=majority';
+const CONNECTION_URL = process.env.MONGODB_URI || "mongodb://localhost/budget",;
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
